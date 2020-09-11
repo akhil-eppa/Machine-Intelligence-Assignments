7984dbc6b8bfe3a84d63aadc5c2228f4a17bacec
Akhil Eppa <akhil.e2k@gmail.com>
2020-09-11 18:19:14 +0530

    Updated functions: All testcases pass

--- A2/Assignment2.py
+++ A2/Assignment2.py
@@ -22,76 +22,86 @@ NOTE : you are allowed to write other helper functions that you can call in the 
 
 from queue import PriorityQueue
 def DFS(cost, start_point, goals):
-    visited=set()
-    stack=[start_point]
-    path=list()
+    #test whether all goals are valid
+    valid=[i for i in goals if i in range(1,len(cost[0]))]
+    if len(valid)==0:
+        return[]
+    visited=set()#Keep track of visited nodes
+    stack=[start_point]#stack implementation in python
+    paths=dict()#Holds path to each node when computed
+    paths[start_point]=[start_point]#Path of initial node is node itself
+    #While stack is not empty do
     while stack:
-        ver=stack.pop()
+        ver=stack.pop()#pop node from stack
         if ver not in visited:
-            visited.add(ver)
-            path.append(ver)
+            visited.add(ver)#if node not in visited add to visited
+            #If the popped node is the goal return path to that node
             if ver in goals:
-                return path
+                return paths[ver]
+            #explore neighbors of popped node
             for i in range(len(cost[ver])-1,0,-1):
-                
                 if cost[ver][i]>0 and (i not in visited):
-                    stack.append(i)
+                    stack.append(i)#if neighbor and not yet visited push to stack
+                    paths[i]=paths[ver]+[i]#Add path to neighbor node
+    #if path to goal is not found return empty list
+    return []
 
 def UCS(cost, start_point, goals):
-    #Add code for UCS
+    #test whether atleast 1 goal is valid
+    valid=[i for i in goals if i in range(1,len(cost[0]))]
+    if len(valid)==0:
+        return[]
     visited = set()
     q=PriorityQueue()
     q.put((0, start_point, [start_point]))
     while not q.empty(): 
         cum_cost, curr, path = q.get()
-        #print(cum_cost)
-        #print(curr)
-        #print(path)
         visited.add(curr)
         if curr in goals:
             return path
         else:
             children=list()
             row_cost = cost[curr]
-            #print(row_cost)
             for i in range(1, len(row_cost)):
-                if row_cost[i]!=-1:
+                #if row_cost[i]!=-1:
+                if row_cost[i]>0:
                     children.append(i)
             for i in children:
                 if i not in visited:
                     q.put((cum_cost+cost[curr][i], i, path+[i]))
+    return []
                     
 def A_Star(cost, heuristic, start_point, goals):
-    #Add code for A_Star
-    visited = set()
-    diction = dict()
+    #test whether atleast 1 goal is valid
+    valid=[i for i in goals if i in range(1,len(cost[0]))]
+    if len(valid)==0:
+        return[]
+    visited = set()#keeps track of all visited nodes
+    diction = dict()#dict to store paths
     q=PriorityQueue()
-    q.put((0, start_point, [start_point]))
+    q.put((heuristic[start_point],0,heuristic[start_point], start_point, [start_point]))
     while not q.empty():
-        cum_cost, curr, path = q.get()
-        #print(cum_cost)
-        #print(curr)
-        #print(path)
+        print(diction)
+        cum_cost, g, h, curr, path = q.get()
         visited.add(curr)
         if curr in goals:
-            diction[cum_cost]=path
+            diction[g]=path
         else:
             children=list()
             row_cost=cost[curr]
-            #print(row_cost)
             for i in range(1,len(row_cost)):
-                if row_cost[i]!=-1:
+                if row_cost[i]>0:
                     children.append(i)
             for i in children:
                 if i not in visited:
-                    q.put((cum_cost+cost[curr][i]+heuristic[curr], i, path + [i]))
-    #print(diction)
+                    g_new=g+cost[curr][i]
+                    h_new=heuristic[i]
+                    f_new=g_new+h_new
+                    q.put((f_new, g_new, h_new, i, path + [i]))
     return diction.get(min(diction.keys()))
 
 def tri_traversal(cost, heuristic, start_point, goals):
     l = []
-
-
     # t1 <= DFS_Traversal
     # t2 <= UCS_Traversal
     # t3 <= A_star_Traversal
