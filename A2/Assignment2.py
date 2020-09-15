1d5ef701c489b1b2d2a823d4edb8aba10951cc2f
Akhil Eppa <akhil.e2k@gmail.com>
2020-09-15 14:34:20 +0530

    Updated Assignment 2

--- A2/Assignment2.py
+++ A2/Assignment2.py
@@ -1,27 +1,5 @@
-'''
-Function tri_traversal - performs DFS, UCS and A* traversals and returns the path for each of these traversals 
-
-n - Number of nodes in the graph
-m - Number of goals ( Can be more than 1)
-1<=m<=n
-Cost - Cost matrix for the graph of size (n+1)x(n+1)
-IMP : The 0th row and 0th column is not considered as the starting index is from 1 and not 0. 
-Refer the sample test case to understand this better
-
-Heuristic - Heuristic list for the graph of size 'n+1' 
-IMP : Ignore 0th index as nodes start from index value of 1
-Refer the sample test case to understand this better
-
-start_point - single start node
-goals - list of size 'm' containing 'm' goals to reach from start_point
-
-Return : A list containing a list of all traversals [[],[],[]]
-
-NOTE : you are allowed to write other helper functions that you can call in the given fucntion
-'''
-
 from queue import PriorityQueue
-def DFS(cost, start_point, goals):
+def DFS_Traversal(cost, start_point, goals):
     #test whether all goals are valid
     valid=[i for i in goals if i in range(1,len(cost[0]))]
     if len(valid)==0:
@@ -46,7 +24,7 @@ def DFS(cost, start_point, goals):
     #if path to goal is not found return empty list
     return []
 
-def UCS(cost, start_point, goals):
+def UCS_Traversal(cost, start_point, goals):
     #test whether atleast 1 goal is valid
     valid=[i for i in goals if i in range(1,len(cost[0]))]
     if len(valid)==0:
@@ -69,8 +47,8 @@ def UCS(cost, start_point, goals):
                 if i not in visited:
                     q.put((cum_cost+cost[curr][i], i, path+[i]))
     return []
-                    
-def A_Star(cost, heuristic, start_point, goals):
+
+def A_star_Traversal(cost, heuristic, start_point, goals):
     #test whether atleast 1 goal is valid
     valid=[i for i in goals if i in range(1,len(cost[0]))]
     if len(valid)==0:
@@ -78,9 +56,9 @@ def A_Star(cost, heuristic, start_point, goals):
     visited = set()#keeps track of all visited nodes
     diction = dict()#dict to store paths
     q=PriorityQueue()
-    q.put((heuristic[start_point],0,heuristic[start_point], start_point, [start_point]))
+    q.put((heuristic[start_point], start_point, 0,heuristic[start_point], [start_point]))
     while not q.empty():
-        cum_cost, g, h, curr, path = q.get()
+        cum_cost, curr, g, h, path = q.get()
         visited.add(curr)
         if curr in goals:
             diction[g]=path
@@ -95,8 +73,32 @@ def A_Star(cost, heuristic, start_point, goals):
                     g_new=g+cost[curr][i]
                     h_new=heuristic[i]
                     f_new=g_new+h_new
-                    q.put((f_new, g_new, h_new, i, path + [i]))
+                    q.put((f_new, i, g_new, h_new, path + [i]))
     return diction.get(min(diction.keys()))
+
+'''
+Function tri_traversal - performs DFS, UCS and A* traversals and returns the path for each of these traversals 
+
+n - Number of nodes in the graph
+m - Number of goals ( Can be more than 1)
+1<=m<=n
+Cost - Cost matrix for the graph of size (n+1)x(n+1)
+IMP : The 0th row and 0th column is not considered as the starting index is from 1 and not 0. 
+Refer the sample test case to understand this better
+
+Heuristic - Heuristic list for the graph of size 'n+1' 
+IMP : Ignore 0th index as nodes start from index value of 1
+Refer the sample test case to understand this better
+
+start_point - single start node
+goals - list of size 'm' containing 'm' goals to reach from start_point
+
+Return : A list containing a list of all traversals [[],[],[]]
+1<=m<=n
+cost[n][n] , heuristic[n][n], start_point, goals[m]
+
+NOTE : you are allowed to write other helper functions that you can call in the given fucntion
+'''
 
 def tri_traversal(cost, heuristic, start_point, goals):
     l = []
@@ -106,9 +108,9 @@ def tri_traversal(cost, heuristic, start_point, goals):
     t1=list()
     t2=list()
     t3=list()
-    t1=DFS(cost, start_point, goals)
-    t2=UCS(cost, start_point, goals)
-    t3=A_Star(cost, heuristic, start_point, goals)
+    t1=DFS_Traversal(cost, start_point, goals)
+    t2=UCS_Traversal(cost, start_point, goals)
+    t3=A_star_Traversal(cost, heuristic, start_point, goals)
     l.append(t1)
     l.append(t2)
     l.append(t3)
